6102a290d0747ae090a3eb59e4217a76b55ae8e8
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-08-16 17:14:51 +1000

    Add conditions: input positive integer only for item cost

--- exp.py
+++ exp.py
@@ -9,10 +9,20 @@ items = int(items)
 number_of_items = 0
 
 while number_of_items < items:
-    print('\n')
+    print('\n')    
     print("Item " + str(number_of_items + 1))
-    item_cost = input("Please enter the cost for this item: ")
-    item_cost = float(item_cost)
+    
+    while True:
+        item_cost = input("Please enter the cost for this item: ")
+        try:
+            item_cost = float(item_cost)
+            if item_cost < 0:
+                print("%d is not positive number." % item_cost)
+                continue
+            break
+        except:
+            print("Please enter a real number")
+     
     item_used = input('How many of this item were used? ')
     item_used = int(item_used)
     tax_deductable = input("Is this item tax deductible (Yes|No)? ")

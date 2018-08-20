f36b927f2bd54d9733481d968ebdae6e0eaa71e6
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-08-21 09:31:12 +1000

    minor corrections

--- exp.py
+++ exp.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-This program that allows the user to choose an unlimited number 
+This program allows the user to choose an unlimited number 
 of items to be recorded. It outputs the most expensive item,
 average cost per item, total expenses and total tax deductible.
 """
@@ -11,10 +11,10 @@ def get_non_negative_float(prompt):
     try:
         value = float(input(prompt))
     except ValueError:
-        print("Please enter a real number.")
+        print("Please enter a decimal number.")
         return get_non_negative_float(prompt)
     if value < 0:
-        print("Please enter a positive number.")
+        print("Please enter a non-negative decimal number.")
         return get_non_negative_float(prompt)
     else:
         return value
@@ -24,10 +24,10 @@ def get_non_negative_int(prompt):
     try:
         value = int(input(prompt))
     except ValueError:
-        print("Please enter an integer.")
+        print("Please enter an integer")
         return get_non_negative_int(prompt)
     if value < 0:
-        print("Please enter a non-negative integer.")
+        print("Please enter a non-negative integer")
         return get_non_negative_int(prompt)
     else:
         return value
@@ -35,20 +35,20 @@ def get_non_negative_int(prompt):
 # function to apply tax deduction based on user's yes or no response
 def tax_deductible(question):
     while "the answer is invalid":
-        reply = str(input(question+'(Yes/No): '))
+        reply = str(input(question))
         if reply == "Yes" or reply == "yes":
             tax = item_cost * items_used
             return tax
             break
-        if reply == "No" or reply == "no":
+        elif reply == "No" or reply == "no":
             tax = 0
             return tax
             break
         else:
-            print("Please enter a Yes or No only. ")            
+            print("The options are Yes or No")            
 
 # prompt user for the number of items to be recorded
-items = input("How many items do you wish to record? ")
+items = get_non_negative_int("How many items do you wish to record? ")
 items = int(items)    
 
 # empty expensive items dictionary, items and tax list
@@ -59,20 +59,20 @@ tax_list = []
 # loop through number of items specified by the user
 number_of_items = 0
 while number_of_items < items:
-    print("------------------------------------------")   
+    print("--------------------------------------------")   
     print("Item " + str(number_of_items + 1) + ":")
     
     # call the functions with arguements and assign to new variable
     item_cost = get_non_negative_float("Please enter the cost for this item: ")   
     items_used = get_non_negative_int("How many items used? ")      
-    tax = tax_deductible("Is this item tax deductible? ")
+    tax = tax_deductible("Is this item tax deductible (Yes,No)? ")
     
-    # multiply item cost/used and add result to items or tax list respectively
+    # multiply item cost and items used. Then add result to items or tax list respectively
     total = (item_cost * items_used)
     items_list.append(total)
     tax_list.append(tax) 
     
-    # add expensive items with item number to dictionary
+    # add most expensive item with item number to dictionary
     expensive_items_list.update({number_of_items +1: total})
     
     # most expensive item calculation
@@ -91,16 +91,14 @@ while number_of_items < items:
     if number_of_items == items:
         break
     
-# main method to print results
-def main():
-    print("+----------------------------------------+")
+# function to print results
+def print_results():
+    print("+------------------------------------------+")
     print("Most expensive item:    ", '$%.2f' % expensive_items_list[maximum], "  Item:",maximum)
     print("Average cost per item:  ", '$%.2f' % average_cost_per_item)
     print("Total expenses:         ", '$%.2f' % total_expenses)
     print("Total tax deductible:   ", '$%.2f' % total_tax_deductible)    
-    print("+----------------------------------------+")
-# Execute main() function
-if __name__ == '__main__':
-    main()
+    print("+------------------------------------------+")
 
+print_results()
  
